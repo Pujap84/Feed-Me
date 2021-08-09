60208f491890b9a866ed4a1ebd6c1f642a8e23aa
Pujap84 <81335331+Pujap84@users.noreply.github.com>
2021-08-09 17:33:14 +1000

    recipe css all done

--- client/js/recipe_results.js
+++ client/js/recipe_results.js
@@ -1,35 +1,38 @@
 function renderRecipeResults(event) {
-  event.preventDefault();
-  const ingredientsArray = state.fridgeItems.map((ingredient) => {
-    return ingredient.name;
-  });
-  const data = { ingredients: ingredientsArray };
-  axios
-    .post("/api/spoonacular/recipes", data)
-    .then((response) => {
-      console.log(response);
-      document.querySelector("#page").innerHTML = `
-                <h1>Your Yummy Recipes</h1>
+    event.preventDefault();
+    const ingredientsArray = state.fridgeItems.map((ingredient) => {
+        return ingredient.name;
+    });
+    const data = { ingredients: ingredientsArray };
+    axios
+        .post("/api/spoonacular/recipes", data)
+        .then((response) => {
+            console.log(response);
+            document.querySelector("#page").innerHTML = `
+                <h1 class="recipe-result-heading">Your Yummy Recipes</h1>
                 <div class="grid-container">
                     ${recipeResults(response.data)}
                 </div>
             `;
-    })
-    .catch((error) => {
-      console.log(error.response);
-      document.querySelector("#errors").innerHTML = error.response.data.message;
-    });
+        })
+        .catch((error) => {
+            console.log(error.response);
+            document.querySelector("#errors").innerHTML =
+                error.response.data.message;
+        });
 }
 
 function recipeResults(results) {
-  return results
-    .map(
-      (result) => `
-              <div onClick="renderIndividualRecipe(event)" data-id=${result.id} class="recipe_search">
-                  <p>${result.title}</p>
-                  <img src="${result.image}" alt="">
-              </div>
+    return results
+        .map(
+            (result) => `
+                <div onClick="renderIndividualRecipe(event)" data-id=${result.id} class="single-recipe">
+                    <div class="recipe-info">
+                        <h3 class="recipe-title">${result.title}</h3>
+                    </div>
+                    <img class="recipe-image" src="${result.image}" alt="">
+                </div>
         `
-    )
-    .join("");
+        )
+        .join("");
 }
